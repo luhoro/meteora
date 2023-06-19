a90131c2e45ece2cec009db5fcbd5ade0975e9fb
luhoro <luisashorowitz@gmail.com>
2023-06-19 17:04:35 -0300

    add header structure

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html, js}"],
+  theme: {
+    extend: {
+      colors: {
+        'gray-prim': '#CCCCCC',
+        'green-prim': '#C7E574',
+        'purple-prim': '#9353FF',
+        'red-sec': '#EE6471',
+        'orange-sec': '#F87F46',
+      }
+    },
+  },
+  plugins: [
+    {
+    tailwindcss: {},
+    autoprefixer: {},
+    },
+  ]
+}
+
