7595c184620a94e9ee32d91ee97179c9fea6c5e6
luhoro <luisashorowitz@gmail.com>
2023-06-20 18:06:11 -0300

    initial base structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,10 @@ module.exports = {
     extend: {
       colors: {
         'gray-prim': '#CCCCCC',
-        'green-prim': '#C7E574',
-        'purple-prim': '#9353FF',
-        'red-sec': '#EE6471',
-        'orange-sec': '#F87F46',
+        'green-prim': '#D1F079',
+        'purple-prim': '#AF80FF',
+        'red-sec': '#FD7D8A',
+        'orange-sec': '#FC9B6D',
       },
       fontFamily: {
         'inter': ['Inter', 'sans-serif']
