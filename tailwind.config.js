837290a9ffbee2f1909ea0a2135ebf6bd95acaa2
luhoro <luisashorowitz@gmail.com>
2023-06-28 16:44:42 -0300

    style newsletter modal

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,9 @@ module.exports = {
       },
       fontFamily: {
         'inter': [ 'Inter', 'sans-serif' ]
+      },
+      boxShadow: {
+        "modal": "0px 0px 20px 0px #00000050" 
       }
     },
   },
